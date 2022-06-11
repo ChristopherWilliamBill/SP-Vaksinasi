0b022cf3b02492bcfedb2ca036c1d636e5cf96eb
mariorenaldyy <92909618+mariorenaldyy@users.noreply.github.com>
2022-06-11 13:29:22 +0700

    Update RekapDosisVaksin.sql

--- RekapDosisVaksin.sql
+++ RekapDosisVaksin.sql
@@ -1,6 +1,34 @@
 alter procedure RekapDosisVaksin
-	@dosis int
+	@dosis1 bit,
+	@dosis2 bit,
+	@dosis3 bit
 as
-	select Masyarakat.IdM, nama
-	from Sertifikat join Masyarakat on Sertifikat.IdM = Masyarakat.IdM
-	where Sertifikat.noDosis = @dosis
+	declare @sql nvarchar(1000)
+
+	set @sql = 'select Masyarakat.IdM, nama, noDosis
+		from Sertifikat join Masyarakat on Sertifikat.IdM = Masyarakat.IdM where'
+
+	if @dosis1 = 0 and @dosis2 = 0 and @dosis3 = 0
+	begin
+		set @sql = left(@sql, len(@sql)-5)
+	end
+	else
+	begin
+		if @dosis1 = 1
+		begin
+			set @sql = CONCAT(@sql, ' noDosis = 1', ' or ')
+		end
+		if @dosis2 = 1
+		begin
+			set @sql = CONCAT(@sql, ' noDosis = 2', ' or ')
+		end
+		if @dosis3 = 1
+		begin
+			set @sql = CONCAT(@sql, ' noDosis = 3', ' or ')
+		end
+		set @sql = left(@sql, len(@sql)-3)
+	end
+
+	EXEC sp_executesql @sql
+
+exec RekapDosisVaksin 1,0,1
